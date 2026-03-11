1f5f1ebde744a2864fb9f17f8bbdbb842d94b05f
Darren Chang <darrenchang951@gmail.com>
2026-03-11 01:07:59 -0700

    Fix: disable lintian for kronosnet

--- packages/kronosnet/autobuild.sh
+++ packages/kronosnet/autobuild.sh
@@ -7,4 +7,12 @@ echo "This is $PKGNAME build scripts"
 . ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME
+
+arch=`arch`
+if [[ "$arch" == "loongarch64" || "$arch" == "aarch64" ]];then
+  for i in `cat $SCRIPT_DIR/series`; do
+   patch -p1 < ../$i
+  done
+fi
+
 exec_build_make
