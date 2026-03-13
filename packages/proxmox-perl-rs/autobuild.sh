d1991b1378694b145a73082ad128d6ef4403b256
Darren Chang <darrenchang951@gmail.com>
2026-03-13 02:13:05 -0700

    Update modules

--- packages/proxmox-perl-rs/autobuild.sh
+++ packages/proxmox-perl-rs/autobuild.sh
@@ -6,9 +6,17 @@ echo "This is $PKGNAME build scripts"
 
 . ../common.sh
 
-for i in common/pkg  pve-rs;
+for i in common/pkg pmg-rs pve-rs;
 do
 	cd $SCRIPT_DIR/$PKGNAME/$i
 	exec_build_make
 	cp *.buildinfo *.changes *.dsc *.tar* *.deb $SCRIPT_DIR/$PKGNAME/ ||true
+  mkdir -p "/tmp/proxmox/perl-rs/${i}/";
+  pwd; ls
+  echo $SCRIPT_DIR/$PKGNAME/${i}
+  for deb_file in $(find $SCRIPT_DIR/$PKGNAME/${i} -name '*.deb'); do
+    cp $deb_file "/tmp/proxmox/perl-rs/${i}/";
+    # dpkg -i *.deb || true
+  done
+  # apt install -y --allow-downgrades $(ls /tmp/proxmox/${pkg_name}/*.deb | grep -v -- '-dbgsym_\|zfs-dracut_\|-test_');
 done
--- packages/spiceterm/autobuild.sh
+++ packages/spiceterm/autobuild.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-PKGNAME="smartmontools"
-
-errlog(){
-        echo $1
-        exit 1
-
-}
-
-exec_build(){
-        apt update
-        yes |mk-build-deps --install --remove
-        echo "clean "
-        make clean || echo ok
-        echo "build deb in `pwd` "
-	cd $SH_DIR/$PKGNAME
-	make dsc || echo ok
-        make deb|| echo "ok"
-}
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PKGNAME=$(basename $SCRIPT_DIR)
 
 echo "This is $PKGNAME build scripts"
 export dscflag=nodsc
@@ -24,5 +8,7 @@ export dscflag=nodsc
 SH_PATH=$(realpath "$0")
 SH_DIR=$(dirname $SH_PATH)
 
+. ../common.sh
+
 cd $SH_DIR/$PKGNAME
-exec_build
+exec_build_make
