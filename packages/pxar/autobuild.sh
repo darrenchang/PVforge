0ed08381b68bdbe15fd192f1f43832fd68bba3ca
Darren Chang <darrenchang951@gmail.com>
2026-03-13 19:13:32 -0700

    Fix: patch pxar so it doesnt check for rust-endian-trait
    - rust endian traits is only used for running tests, so its ok to
    ignore it

---
+++ packages/pxar/autobuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+PKGNAME=$(basename $SCRIPT_DIR)
+export DEB_BUILD_PROFILES=nocheck
+
+echo "This is $PKGNAME build scripts"
+
+. ../common.sh
+
+cd $SCRIPT_DIR/$PKGNAME
+
+arch=`arch`
+if [[ "$arch" == "loongarch64" || "$arch" == "aarch64" ]];then
+  for i in `cat $SCRIPT_DIR/series`; do
+   patch -p1 < ../$i
+  done
+fi
+yes |mk-build-deps --install --remove
+cd $SCRIPT_DIR/$PKGNAME
+exec_build_make
