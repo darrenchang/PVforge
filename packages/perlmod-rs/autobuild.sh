e38c4595279a68484b7cf8097f6c28f348d4c8f8
Darren Chang <darrenchang951@gmail.com>
2026-03-15 19:38:39 -0700

    Add librust-perlmod

--- packages/perlmod-rs/autobuild.sh
+++ packages/perlmod-rs/autobuild.sh
@@ -4,39 +4,24 @@ PKGNAME=$(basename $SCRIPT_DIR)
 
 echo "This is $PKGNAME build scripts"
 
-. ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME
 
-arch=`arch`
-if [[ "$arch" == "loongarch64" || "$arch" == "aarch64" ]];then
-  for i in `cat $SCRIPT_DIR/series`; do
-   patch -p1 < ../$i
-  done
-fi
-
 RUST_LIBS=(
-    "cc"
-    "endian-trait-derive"
-    "endian-trait"
+    "perlmod-macro"
+    "perlmod"
   )
 
 for rust_lib in "${RUST_LIBS[@]}"; do
   rm -rf ./build/*
-  ./repackage.sh ${rust_lib//-/_}
-  (
-    cd build/${rust_lib}/
-    pwd
-    yes |mk-build-deps --install --remove
-    dpkg-buildpackage -b -us -uc
-  )
+  ./build.sh ${rust_lib}
   mkdir -p /tmp/${PKGNAME}-temp
   mkdir -p /tmp/${PKGNAME}
   for deb_pkg in $(find $(pwd)/build/ -name *.deb); do
     cp ${deb_pkg} "/tmp/${PKGNAME}-temp/"
     cp ${deb_pkg} "/tmp/${PKGNAME}/"
-    apt install -y --allow-downgrades /tmp/${PKGNAME}-temp/*.deb --reinstall
   done
+  apt install -y --allow-downgrades /tmp/${PKGNAME}-temp/*.deb --reinstall
   rm -rf /tmp/${PKGNAME}-temp
 done
 
