9d420c619fd87582f50e58642fbf4428af945b27
Darren Chang <darrenchang951@gmail.com>
2026-05-17 12:50:19 -0700

    Add dev script

---
+++ quick-run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+export BUILDER_CONTAINER=pve_builder;
+export PACKAGE_NAME=proxmox-kernel-helper
+
+# Parse named arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --builder_name) export BUILDER_CONTAINER="$2"; shift ;;
+        --package_name) export PACKAGE_NAME="$2"; shift ;;
+        --help)
+            echo "Usage: $0 [--builder_name \"pve_builder\"]"
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+docker exec -ti ${BUILDER_CONTAINER} bash -c "rm -rf /tmp/${PACKAGE_NAME}*";
+docker exec -ti ${BUILDER_CONTAINER} bash -c "rm -rf /workspace/packages/${PACKAGE_NAME}/";
+docker cp ./packages/${PACKAGE_NAME}/ ${BUILDER_CONTAINER}:/workspace/packages/${PACKAGE_NAME}/;
+docker exec -ti ${BUILDER_CONTAINER} bash -c "/workspace/packages/build_all.sh --package ${PACKAGE_NAME}" 2>&1 | tee log.txt
+
