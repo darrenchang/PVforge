2cf6a1fa1e074d2072edb29dad2bd8b54f12bc7e
Darren Chang <darrenchang951@gmail.com>
2026-06-14 18:57:14 -0700

    Fix proxmox-archive-keyring compile issue

--- packages/proxmox-archive-keyring/autobuild.sh
+++ packages/proxmox-archive-keyring/autobuild.sh
@@ -7,5 +7,10 @@ echo "This is $PKGNAME build scripts"
 . ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME
-cp ../pveport.gpg debian/
+
+for i in `cat $SCRIPT_DIR/series`;
+  do patch -p1 < ../$i
+done
+
+cp ../lierfang.gpg debian/
 exec_build_make
