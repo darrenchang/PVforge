1e04e830ef6c2daa937cb3c58645f5dffe7723d7
Darren Chang <darrenchang951@gmail.com>
2026-06-24 02:09:01 -0700

    Add pve-installer deps

--- packages/pve-installer/autobuild.sh
+++ packages/pve-installer/autobuild.sh
@@ -3,8 +3,11 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 PKGNAME=$(basename $SCRIPT_DIR)
 
 echo "This is $PKGNAME build scripts"
+apt update && \
+apt install -y geoip-bin libgtk3-perl;
 
 . ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME
+git config --global --add safe.directory $(pwd)
 exec_build_make
