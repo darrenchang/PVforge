ffc3024d37afa9b92b57528188f94ee9027adbc0
Darren Chang <darrenchang951@gmail.com>
2026-07-09 00:18:43 -0700

    Update proxmox-backup and proxmox-backup-qemu

--- packages/proxmox-backup-qemu/autobuild.sh
+++ packages/proxmox-backup-qemu/autobuild.sh
@@ -7,7 +7,13 @@ echo "This is $PKGNAME build scripts"
 
 . ../common.sh
 
+cd $SCRIPT_DIR/$PKGNAME
+git config --global --add safe.directory $SCRIPT_DIR/$PKGNAME
+git submodule deinit -f submodules/proxmox-backup/
+git submodule update --init --recursive
+
 cd $SCRIPT_DIR/$PKGNAME/submodules/proxmox-backup/
+git checkout 6d515b2625d235360199f299da0cfff68f240c86
 apt update
 yes |mk-build-deps --install --remove
 apt install librust-pbs-api-types-dev=1.0.1-1 -y
