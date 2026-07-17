c24555f37c855bfa2f532c97e6d2a169ffc745bc
Darren Chang <darrenchang951@gmail.com>
2026-07-17 00:05:04 -0700

    Fix: qemu-server build script

--- packages/qemu-server/autobuild.sh
+++ packages/qemu-server/autobuild.sh
@@ -7,4 +7,15 @@ echo "This is $PKGNAME build scripts"
 . ../common.sh
 
 cd $SCRIPT_DIR/$PKGNAME
+
+rm -fr $SCRIPT_DIR/$PKGNAME/.git
+git init
+git config --global --add safe.directory $(pwd) && \
+git remote add origin https://git.proxmox.com/git/qemu-server && \
+git remote set-url --push origin https://git.proxmox.com/git/qemu-server && \
+git fetch origin && \
+git checkout -f master && \
+git submodule update --init --recursive && \
+sed -i '/pve-ha-manager <!nocheck>/d' $SCRIPT_DIR/$PKGNAME/debian/control;
+
 exec_build_make
